d97be79ef6ee323cf752804b377d13e3bd607fde
Carlos Molina <carlosjmr12@gmail.com>
2019-07-30 00:19:05 -0400

    07/30/19
    cambio en comentario

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SliderPage(),
+      home: SliderPage(), //aqui coloco la principal
       
     );
   }
